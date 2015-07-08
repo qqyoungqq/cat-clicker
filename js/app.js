6b189053e30c2d30617da02334b52e6ab305546d
qqyoungqq <qianyang@mail.usf.edu>
2015-07-08 14:18:34 -0400

    feat: display names of two cats and show the picture of clicked cat

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
-var clickNum=0;
-var catelem = document.createElement("div");
-var content = document.createTextNode('Number of clicks is ' + clickNum);
-catelem.appendChild(content);
-document.body.appendChild(catelem);
+var catModel = {
+	cats : [
+		{
+			name : "Bubbles",
+			clickCount : 0,
+			imgSrc : "bubbles.jpg"
+		},
+		{
+			name : "Echo",
+			clickCount : 1,
+			imgSrc : "echo.jpg"
+		}
+	]
+};
 
-var elem = document.getElementById('cat-img');
-elem.addEventListener('click', function() {
-	clickNum++;
-	catelem.innerHTML = "Number of click is " + clickNum;
-	},false);
+for (catind in catModel.cats) {
+	$("#cat-names").append("<h4>"+catModel.cats[catind].name+"</h4>");
+}
+
+function getInd(catname) {
+	for (var i = 0, len = catModel.cats.length; i < len; i++) {
+		if (catname == catModel.cats[i].name) {
+			return i;
+		}
+	}
+}
+
+
+$("h4").click(function() {
+	var catname=this.innerHTML;
+	var ind = getInd(catname);
+	console.log(ind);
+	var imgsource = catModel.cats[ind].imgSrc;
+	var content = '<img id="cat-imgs"' + 'src="' + imgsource +'" alt="cat image">';
+	$("img").replaceWith(content);
+});
