4409fd0a48eb81bf79599836d1458dc8471c3598
qqyoungqq <qianyang@mail.usf.edu>
2015-07-09 12:48:55 -0400

    feat: Count number of click cumulatively for each cat

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ var catModel = {
 for (catind in catModel.cats) {
 	$("#cat-names").append("<h4>"+catModel.cats[catind].name+"</h4>");
 }
-$("#cat-counts").append("<h5>0</h5>");
+$("#cat-counts").append("<h5></h5>");
 
 function getInd(catname) {
 	for (var i = 0, len = catModel.cats.length; i < len; i++) {
@@ -36,18 +36,15 @@ $("h4").click(function() {
 	var imgsource = catModel.cats[ind].imgSrc;
 	patCount = catModel.cats[ind].clickCount;
 	var imgContent = '<img id="cat-imgs" class="catpic"' + ' src="' + imgsource +'" alt="cat image">';
+	var countContent = '<h5>'+catModel.cats[ind].clickCount+'</h5>';
+	$("h5").replaceWith(countContent);
 	$("img").replaceWith(imgContent);
 });
 
-//$("img").on('click', 'img.test', function() {
-	//patCount++;
-//	console.log('haha');
-//	//$("h5").replaceWith(patCount);
-//});
 
 $( document ).on( 'click', '.catpic', function () {
-   patCount++;
-   console.log(patCount);
-   var countContent = '<h5>'+patCount+'</h5>';
-   $("h5").replaceWith(countContent);
+	catModel.cats[ind].clickCount++;
+	console.log(catModel.cats[ind].clickCount);
+	var countContent = '<h5>'+catModel.cats[ind].clickCount+'</h5>';
+	$("h5").replaceWith(countContent);
 });
